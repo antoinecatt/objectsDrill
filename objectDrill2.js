e527288db9c95dedb9ebd7e645a7a200dc52fad4
kathyleboeuf <kleboeuf@townisp.com>
2018-07-18 14:57:05 -0400

    arrays in objects added

--- objectDrill2.js
+++ objectDrill2.js
@@ -13,8 +13,13 @@ const loopObj = {
   fum: 11,
   quux: () => 'hi',
   spam: [1,2,3,4,5]
-}
+};
 
 for(let key in loopObj) {
   console.log(`${key}: ${loopObj[key]}`)
 }
+
+const menu = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+console.log(menu.meals[3]);
